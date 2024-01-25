9c0b128e541e5abe2a63f419b429ed6b8ec559ec
VinhLee <vinhlengabay@gmail.com>
2024-01-25 14:54:40 +0700

    N07 - Lab4

--- PC06029_LePhuocVInh_Lab4/App/Core/Router.php
+++ PC06029_LePhuocVInh_Lab4/App/Core/Router.php
@@ -39,29 +39,3 @@ class Router
         }
     }
 }
-
-
-class ParentClass
-{
-    protected $parentMsg = "protected parent attribute<br>";
-
-    public function parentDisplay()
-    {
-        echo "protected parent method<br>";
-        echo $this->parentMsg;
-    }
-}
-
-class ChildClass extends ParentClass
-{
-    protected $childMsg = "Protected Child attribute.<br>";
-
-    public function childDisplay()
-    {
-        echo "Public Child method to display protected parent members:<br>";
-        $this->parentDisplay();
-    }
-}
-
-$child = new ParentClass();
-$child->parentDisplay();
